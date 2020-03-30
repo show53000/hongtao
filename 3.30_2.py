3df14294526a79a98774c339cee85f7ec710d516
show53000 <hongtao.vip@hotmail.com>
2020-03-30 20:08:59 +0800

    3.30 add new code

---
+++ 3.30_2.py
@@ -0,0 +1,48 @@
+#！/usr/bin/nev python
+#-*-coding:utf-8 -*-
+#Author :show530
+
+# alien_0 = {'color':'green','points':5}
+# alien_1 = {'color':'yellow','points':10}
+# alien_2 = {'color':'red','points':15}
+#
+# aliens = [alien_0,alien_1,alien_2]
+#
+# for alien in aliens:
+#     print(alien)
+
+aliens = []
+
+# # 创建30个绿色的外星人
+# for alien_number in range(30):
+#     new_alien = {'color':'green','points':5,'speed':'slow'}
+#     aliens.append(new_alien)
+#
+# # 显示前5个外星人
+# for alien in aliens[:5]:
+#     print(alien)
+# print("...")
+#
+# # 显示创建了多少个外星人
+# print("Total number of aliens: " + str(len(aliens)))
+
+
+# 创建一个用于存储外星人的空列表
+aliens = []
+
+# 创建30个绿色的外星人
+for alien_number in range(0,30):
+    new_alien = {'color':'green','points':5,'speed':'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+
+# 显示前五个外星人
+for alien in aliens[0:5]:
+    print(alien)
+
+print("...")

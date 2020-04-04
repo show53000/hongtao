c312b068296b682b05ccf4643f9cb839d54ddaf5
show53000 <hongtao.vip@hotmail.com>
2020-04-04 18:20:12 +0800

    python_study

--- 4.4.py
+++ 4.4.py
@@ -24,7 +24,12 @@ class Car():
 class ElectricCar(Car):
     def __init__(self,make,model,year):
         super().__init__(make,model,year)
+        self.battery_size = 70
+    def discrib_battery(self):
+        print("This car has " + str(self.battery_size)+"-kWh battery.")
+
 
 my_tesla = ElectricCar('tesla','model S',2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.discrib_battery()
 
---
+++ 4.4_1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+#!coding = UTF-8
+# Author = show530
+
+
+class Car():
+    def __init__(self,make,model,):

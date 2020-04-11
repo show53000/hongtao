50b6ef97c07cf9e567e85fdc7afc119f77274e3e
show53000 <hongtao.vip@hotmail.com>
2020-04-11 10:04:15 +0800

    python_study

--- 4.7.py
+++ 4.7.py
@@ -12,14 +12,20 @@
 # print(pi_string)
 # print(len(pi_string))
 
-
+#frist
 filename = 'learning_python.txt'
 with open(filename) as file_object:
+    print(file_object.read())
+# #second
+with open(filename) as file_object:
     lines = file_object.readlines()
-lib = []
+learning_string =''
 for line in lines:
-    lib.append(line)
-
-print(lib)
-print(len(lib))
-
+    learning_string += line.strip()+' '
+print(learning_string)
+print(len(learning_string))
+#third
+with open(filename) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip())

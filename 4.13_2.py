9c2a9316ad71479575d376c545ae4dc14f6a21a5
show53000 <hongtao.vip@hotmail.com>
2020-04-14 15:43:23 +0800

    python_study

--- 4.13_2.py
+++ 4.13_2.py
@@ -3,10 +3,12 @@
 #Author :show530
 
 import pandas as pd
+path = "D:/日常工作/发布站相关/发布站数据/baidu413.csv"
+pd.set_option('display.unicode.ambiguous_as_wide', True)
+pd.set_option('display.unicode.east_asian_width', True)
+pd.set_option('display.max_columns',None)
+pd.set_option('display.width',5000)
+pd.set_option('mode.chained_assignment',None)
+date = pd.read_csv(path,delimiter="\t",encoding='gbk',header=6)
+print(date.head(10))
 
-path = "D:/日常工作/发布站相关/发布站数据/new_word_report_20200412-20200412_661758.csv"
-
-date = pd.read_csv(path,encoding='gbk',error_bad_lines=False)
-csv_date = date.drop(date.index[4])
-df = pd.DataFrame(csv_date)
-print(df)
